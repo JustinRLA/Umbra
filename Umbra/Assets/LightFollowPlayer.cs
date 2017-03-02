8e7fd31deaf75fa48663164c028233ab31e67d72
Justin Loranger-Ahluwalia <lorangera.justin@gmail.com>
2017-03-02 15:11:56 -0500

    lightfollowplatyer

---
+++ Umbra/Assets/LightFollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollowPlayer : MonoBehaviour {
+	public GameObject PlayerMy;
+	// Use this for initialization
+	void Start () {
+		PlayerMy = GameObject.Find ("2DCharacter");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = PlayerMy.transform.position;
+	}
+}
