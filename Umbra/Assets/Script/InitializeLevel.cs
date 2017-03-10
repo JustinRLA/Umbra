3c1cd0af09fb1b467fdba5188f0ddf980f27efe3
Justin Loranger-Ahluwalia <lorangera.justin@gmail.com>
2017-03-10 11:57:06 -0500

    SpawnSystem

--- Umbra/Assets/Script/InitializeLevel.cs
+++ Umbra/Assets/Script/InitializeLevel.cs
@@ -7,45 +7,111 @@ public class InitializeLevel : MonoBehaviour {
 	public GameObject ThePLayer;
 	public Transform[] SpawnPoint;
 	public GameObject[] EnnemyPack;
+	public GameObject Cam;
+	public GameObject PlayerLight;
+	public GameObject[] LightCam;
 
 
+	void Awake()
+	{
+		ThePLayer = (GameObject)Resources.Load ("2DCharacter",typeof (GameObject));
 
-
+	if (PlayerPrefs.GetInt ("SaveSystem")==0)
+		Instantiate (ThePLayer, SpawnPoint [0].position, SpawnPoint [0].rotation);
+	if (PlayerPrefs.GetInt ("SaveSystem")==1)
+		Instantiate (ThePLayer, SpawnPoint [1].position, SpawnPoint [1].rotation);
+	if (PlayerPrefs.GetInt ("SaveSystem")==2)
+		Instantiate (ThePLayer, SpawnPoint [2].position, SpawnPoint [2].rotation);
+	if (PlayerPrefs.GetInt ("SaveSystem")==3)
+		Instantiate (ThePLayer, SpawnPoint [3].position, SpawnPoint [3].rotation);
+	if (PlayerPrefs.GetInt ("SaveSystem")==4)
+		Instantiate (ThePLayer, SpawnPoint [4].position, SpawnPoint [4].rotation);
+	if (PlayerPrefs.GetInt ("SaveSystem")==5)
+		Instantiate (ThePLayer, SpawnPoint [5].position, SpawnPoint [5].rotation);
+	if (PlayerPrefs.GetInt ("SaveSystem")==6)
+		Instantiate (ThePLayer, SpawnPoint [6].position, SpawnPoint [6].rotation);
+	}
 	// Use this for initialization
 	void Start () {
-		ThePLayer = (GameObject)Resources.Load ("2DCharacter",typeof (GameObject));
-		GameManager = GameObject.Find ("CheckPointManager");
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 0)
-			Instantiate (ThePLayer, SpawnPoint [0].position, SpawnPoint [0].rotation);
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 1)
-			Instantiate (ThePLayer, SpawnPoint [1].position, SpawnPoint [1].rotation);
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 2)
-			Instantiate (ThePLayer, SpawnPoint [2].position, SpawnPoint [2].rotation);
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 3)
-			Instantiate (ThePLayer, SpawnPoint [3].position, SpawnPoint [3].rotation);
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 4)
-			Instantiate (ThePLayer, SpawnPoint [4].position, SpawnPoint [4].rotation);
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 5)
-			Instantiate (ThePLayer, SpawnPoint [5].position, SpawnPoint [5].rotation);
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 6)
-			Instantiate (ThePLayer, SpawnPoint [6].position, SpawnPoint [6].rotation);
+	//	Cam=GameObject.Find("CamBaseTwo");
+//		ThePLayer = (GameObject)Resources.Load ("2DCharacter",typeof (GameObject));
+		//GameManager = GameObject.Find ("CheckPointManager");
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 0)
+//			Instantiate (ThePLayer, SpawnPoint [0].position, SpawnPoint [0].rotation);
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 1)
+//			Instantiate (ThePLayer, SpawnPoint [1].position, SpawnPoint [1].rotation);
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 2)
+//			Instantiate (ThePLayer, SpawnPoint [2].position, SpawnPoint [2].rotation);
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 3)
+//			Instantiate (ThePLayer, SpawnPoint [3].position, SpawnPoint [3].rotation);
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 4)
+//			Instantiate (ThePLayer, SpawnPoint [4].position, SpawnPoint [4].rotation);
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 5)
+//			Instantiate (ThePLayer, SpawnPoint [5].position, SpawnPoint [5].rotation);
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 6)
+//			Instantiate (ThePLayer, SpawnPoint [6].position, SpawnPoint [6].rotation);
+//
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 0)
+//		{
+//			EnnemyPack [0].SetActive (true);
+//	}
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 1)
+//		{
+//			EnnemyPack [1].SetActive (true);
+//		}	
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 2)
+//		{
+//			EnnemyPack [2].SetActive (true);
+//		}
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 3)
+//		{
+//			EnnemyPack [3].SetActive (true);
+//		}
+	PlayerPrefs.SetInt ("SaveSystem",0);
+			
+		Cam.SetActive (true);
+		PlayerLight.SetActive (true);
+		if (PlayerPrefs.GetInt ("SaveSystem")==0)
+				{
+					EnnemyPack [0].SetActive (true);
+				}	
+		if (PlayerPrefs.GetInt ("SaveSystem")==1)
+				{
+					EnnemyPack [1].SetActive (true);
+				}
+		if (PlayerPrefs.GetInt ("SaveSystem")==2)
+				{
+					EnnemyPack [2].SetActive (true);
+				}
 
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 0)
+		if (PlayerPrefs.GetInt ("SaveSystem")==0)
 		{
-			EnnemyPack [0].SetActive (true);
-	}
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 1)
-		{
-			EnnemyPack [1].SetActive (true);
+			LightCam [0].SetActive (true);
 		}	
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 2)
+		if (PlayerPrefs.GetInt ("SaveSystem")==1)
 		{
-			EnnemyPack [2].SetActive (true);
+			LightCam [1].SetActive (true);
 		}
-		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 3)
+		if (PlayerPrefs.GetInt ("SaveSystem")==2)
 		{
-			EnnemyPack [3].SetActive (true);
+			LightCam [2].SetActive (true);
 		}
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 0)
+//		{
+//			EnnemyPack [0].SetActive (true);
+//		}
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 1)
+//		{
+//			EnnemyPack [1].SetActive (true);
+//		}	
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 2)
+//		{
+//			EnnemyPack [2].SetActive (true);
+//		}
+//		if (GameManager.GetComponent<CheckPointState> ().CheckpointState == 3)
+//		{
+//			EnnemyPack [3].SetActive (true);
+//		}
 
 	}
 	
