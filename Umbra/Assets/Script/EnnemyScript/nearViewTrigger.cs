af2f5f03cbca96a77e5ea1c73941f6628975e04f
Justin Loranger-Ahluwalia <lorangera.justin@gmail.com>
2017-03-10 13:33:56 -0500

    imageIntegrationPart1

--- Umbra/Assets/Script/EnnemyScript/nearViewTrigger.cs
+++ Umbra/Assets/Script/EnnemyScript/nearViewTrigger.cs
@@ -6,6 +6,7 @@ public class nearViewTrigger : MonoBehaviour {
 	public GameObject MySight;
 	public GameObject EnnemyBase;
 	public GameObject mySignListener;
+	public bool inSigght;
 
 	// Use this for initialization
 	void Start () {
@@ -14,23 +15,33 @@ public class nearViewTrigger : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-	void OnTriggerEnter2D (Collider2D col) {
-		//print ("fuckkkkkkkkk");
-		if (col.tag == "ViewTrigger" || col.tag == "Player")
+		if(MySight.GetComponent<SightListenerTemplate>().iSeeYou==true && inSigght==true)
 		{
-			if(MySight.GetComponent<SightListenerTemplate>().iSeeYou==true)
-			{
-//			EnnemyBase.GetComponent<EnnnemyPatrolUpgraded> ().timerState = 30;
-//			EnnemyBase.GetComponent<EnnnemyPatrolUpgraded> ().Alert = true;
+			//			EnnemyBase.GetComponent<EnnnemyPatrolUpgraded> ().timerState = 30;
+			//			EnnemyBase.GetComponent<EnnnemyPatrolUpgraded> ().Alert = true;
 			mySignListener.GetComponent <SightListenerTemplate>().throwSuspicious=false;
 
 			mySignListener.GetComponent <SightListenerTemplate>().throwAlert=true;
 			EnnemyBase.GetComponent<EnnnemyPatrolUpgraded> ().timerState = 24;
 
-			print ("fuckkkkkkkkk");
+		}
+
 	}
+	void OnTriggerEnter2D (Collider2D col) {
+		//print ("fuckkkkkkkkk");
+		if (col.tag == "ViewTrigger")
+		{
+			inSigght = true;
+			print ("fuckkkkkkkkk");
+
 		}
 	}
+	void OnTriggerExit2D (Collider2D col) {
+		//print ("fuckkkkkkkkk");
+		if (col.tag == "ViewTrigger")
+		{
+			inSigght = false;
+		}
+	}
+
 }
