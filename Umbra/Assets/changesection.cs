14eaa5a4b98dcec17495f01c04ee6a259ca852ef
Justin Loranger-Ahluwalia <lorangera.justin@gmail.com>
2017-04-01 20:32:20 -0400

    sdjh

---
+++ Umbra/Assets/changesection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changesection : MonoBehaviour {
+	public GameObject LastSection;
+	public GameObject NextSection;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+	if(col.tag=="Player")
+	{
+		NextSection.SetActive (true);
+
+
+
+		LastSection.SetActive (false);
+
+	}
+
+}
+
+}
