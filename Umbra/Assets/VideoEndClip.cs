66cd1cc1d0a01cb23f9ac5a140224f869fbeb067
Justin Loranger-Ahluwalia <lorangera.justin@gmail.com>
2017-04-19 15:38:12 -0400

    Cutscene cont.

--- Umbra/Assets/VideoEndClip.cs
+++ Umbra/Assets/VideoEndClip.cs
@@ -6,10 +6,24 @@ using UnityEngine.UI;
 public class VideoEndClip : MonoBehaviour {
 	public MovieTexture EndFilm;
 
+	//audio
+	private AudioSource audio;
+	//
+
 	// Use this for initialization
 	void Start () {
 		GetComponent<RawImage> ().texture = EndFilm as MovieTexture;
+
+		//audio
+		audio = GetComponent<AudioSource> ();
+		audio.clip = EndFilm.audioClip;
+		//
+
 		EndFilm.Play ();
+
+		//audio
+		audio.Play ();
+		//
 	}
 	
 	// Update is called once per frame
