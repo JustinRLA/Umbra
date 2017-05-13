e2b828ee83ca4b8bc82ab205d0a913ef38700740
frankgreentea <francis.touchettedrolet@hotmail.com>
2017-05-13 17:51:50 -0400

    Revert "reorganisation"
    This reverts commit 51025aa97f0887f10820423bd10a6075fdd2bafe.

---
+++ Umbra/Assets/ChangeConeVision.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeConeVision : MonoBehaviour {
+	public GameObject myEnnemy;
+	EnnnemyPatrolUpgraded myPat;
+	public GameObject ConeAlerte;
+	public GameObject ConeNormal;
+	public GameObject ConeSuspicious;
+
+	// Use this for initialization
+	void Start () {
+		myPat = myEnnemy.GetComponent<EnnnemyPatrolUpgraded> ();
+		InvokeRepeating ("CheckCone",1,0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CheckCone()
+	{
+		if (myPat.Alert == true) {
+			ConeAlerte.SetActive (true);
+			ConeNormal.SetActive (false);
+			ConeSuspicious.SetActive (false);
+		}
+		if (myPat.Suspicious == true) {
+			ConeAlerte.SetActive (false);
+			ConeNormal.SetActive (false);
+			ConeSuspicious.SetActive (true);
+		}
+
+		if (myPat.Suspicious == false && myPat.Alert==false) {
+			ConeAlerte.SetActive (false);
+			ConeNormal.SetActive (true);
+			ConeSuspicious.SetActive (false);
+		}
+
+	}
+	 
+}
